5db2ac8c62ddb0660d582dad1dc2e882f5c2d372
Tobi <adegbuji_tobi@yahoo.com>
2020-12-08 15:27:22 -0500

    Initial commit

---
+++ src/main/java/com/useditems/useditemsbackend/UsedItemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.useditems.useditemsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedItemsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedItemsBackendApplication.class, args);
+    }
+
+}
