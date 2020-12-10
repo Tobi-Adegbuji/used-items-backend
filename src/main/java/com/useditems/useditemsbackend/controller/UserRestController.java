4404132cd5459cbd7c9f226620877d4bf818bfdb
Kai Shu <kaishu83@gmail.com>
2020-12-10 11:16:46 -0700

    ll

--- src/main/java/com/useditems/useditemsbackend/controller/UserRestController.java
+++ src/main/java/com/useditems/useditemsbackend/controller/UserRestController.java
@@ -1,7 +1,7 @@
 package com.useditems.useditemsbackend.controller;
 import com.useditems.useditemsbackend.model.User;
 import com.useditems.useditemsbackend.services.UserService;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
